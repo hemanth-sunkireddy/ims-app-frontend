7a63e7429804f9bb01ec85876234ca762b5c5419
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-06-03 07:34:30 +0530

    display correct error code and message during login

--- src/backend_requests/AuthUser.tsx
+++ src/backend_requests/AuthUser.tsx
@@ -1,23 +1,17 @@
-import { auth_route } from "../constants/APIHandler";
+import { auth_route, domain } from "../constants/APIHandler";
 import { getCookie } from "./CookieManage";
 
 export const authenticate_user = async (
   Email: string,
   _Password: string,
   setErrorText: (text: string) => void,
-  setIsError: (text: boolean) => void,
+  setIsLoading: (text: boolean) => void,
 ): Promise<boolean> => {
   try {
-    console.log("Authenticating User...");
     // Creating Form Data Which is defined in Backend
     const formData = new FormData();
-
     formData.append("username", Email);
     formData.append("password", _Password);
-    console.log("Email: ", Email);
-    console.log("Password: ", _Password);
-    const uri_for_auth = auth_route;
-    console.log("URL FOR AUTHENTICATION: ", uri_for_auth);
 
     const timeoutPromise = new Promise((_, reject) => {
       setTimeout(() => {
@@ -25,7 +19,7 @@ export const authenticate_user = async (
       }, 5000);
     });
 
-    const responsePromise = fetch(uri_for_auth, {
+    const responsePromise = fetch(auth_route, {
       method: "POST",
       headers: {
         Accept: "*/*",
@@ -36,38 +30,35 @@ export const authenticate_user = async (
       body: formData,
     });
 
-    const response = await Promise.race([responsePromise, timeoutPromise]);
-    console.log("Response Data: ", responsePromise);
-    console.log("RESPONSE: ", response);
-    if (!(response as Response).ok) {
-      setIsError(false);
-      setErrorText("Invalid Username or Password");
-      console.log("Error In Recieving User Details");
-      return false;
+    const response = await Promise.race([responsePromise, timeoutPromise]) as Response;
+    if (response.status === 422) {
+      setErrorText(response.status + " " + "unprocessable Entry");
     }
-
-    const responseData = await (response as Response).json();
-    // console.log("RESPONSE From Server: ", response);
-    setIsError(false);
-    // Handle the response
-    if (response.status === 200) {
-      const cookie_status = await getCookie(response, uri_for_auth);
+    else if (response.status === 400) {
+      setErrorText(response.status + " " + "Invalid Username or Password");
+    }
+    else if (response.status === 200) {
+      const cookie_status = await getCookie(response, domain);
       if (cookie_status == false) {
         setErrorText(
           "Authentication Success, Cookie assign failed, Please Click Login again",
         );
+        setIsLoading(false);
         return false;
       }
+      setIsLoading(false);
       return true;
-    } else {
-      setErrorText("Invalid Credentials");
-      console.log("Invalid Credentials");
-      return false;
     }
-  } catch (error) {
-    setIsError(false);
-    setErrorText("Please check your Network Connection");
-    console.log("Failed in Authentication Network request failed ", error);
+    else {
+      setErrorText(response.status + + " " + response.statusText);
+    }
+    setIsLoading(false)
+    return false;
+  }
+  catch (e) {
+    setIsLoading(false);
+    const eror_message = (e as Error).message;
+    setErrorText("Error: " + eror_message);
     return false;
   }
 };
