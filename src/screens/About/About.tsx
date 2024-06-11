08f228724211ea44028cd60ffe76708c46d0765a
bhavberi <bhavberi@users.noreply.github.com>
2024-06-11 17:03:47 +0000

    Apply Prettier Formatting Fixes

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -8,7 +8,10 @@ import {
   StyleSheet,
 } from "react-native";
 import global from "../../styles/global";
-import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   contentContainer: {
@@ -51,21 +54,23 @@ function About(): React.JSX.Element {
             <Text style={styles.description}>
               This app originated as a project for the Design and Analysis of
               Software Systems (DASS) course in the Spring 2024 semester, guided
-              by Prof. Raghu Reddy. It is currently an Android mobile application,
-              with an iOS version coming soon, dedicated to the IMS website of
-              IIIT Hyderabad.
+              by Prof. Raghu Reddy. It is currently an Android mobile
+              application, with an iOS version coming soon, dedicated to the IMS
+              website of IIIT Hyderabad.
             </Text>
             <Text style={styles.description}>
-              Students can use the application to submit leave requests, add bank
-              accounts, and view their attendance, transcripts, and profile
+              Students can use the application to submit leave requests, add
+              bank accounts, and view their attendance, transcripts, and profile
               details, including general information and address. They can also
               check the status of their leave requests and bank applications.
-              Additionally, students can add or edit their bank details until they
-              receive approval.
+              Additionally, students can add or edit their bank details until
+              they receive approval.
             </Text>
           </View>
 
-          <View style={{ flexDirection: "row", paddingLeft: 20, paddingTop: 20 }}>
+          <View
+            style={{ flexDirection: "row", paddingLeft: 20, paddingTop: 20 }}
+          >
             <Text style={{ fontWeight: "bold", color: "black", fontSize: 15 }}>
               Project Codebase:{" "}
             </Text>
