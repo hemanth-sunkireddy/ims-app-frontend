6ff58c35b5ec62d6d703c0093b681566d21125db
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-06-13 23:23:10 +0530

    removing dummy data and unnecessary code in notification screen

--- src/screens/Notifications/Notifications.tsx
+++ src/screens/Notifications/Notifications.tsx
@@ -1,65 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { View, SafeAreaView, ScrollView, Text } from "react-native";
-import notifee from "@notifee/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { onDisplayNotification } from "../../components/SendNotification";
 
 function Notification(): React.JSX.Element {
-  const [presentState, setpresentState] = useState("");
-  const [_storedState, setStoredState] = useState("");
-
-  useEffect(() => {
-    // Retrieve the stored status when the component mounts
-    async function getStoredStatus() {
-      try {
-        const value = await AsyncStorage.getItem("initial_status");
-        if (value !== null) {
-          setStoredState(value);
-          // console.log("Initially, retrived value from the storage and set to storedState", value);
-        }
-      } catch (e) {
-        // console.log(e);
-      }
-    }
-    getStoredStatus();
-  }, []);
-
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("initial_status");
-      // console.log("Already Stored State: ", value);
-      // console.log("Present State: ", presentState);
-
-      if (value !== presentState) {
-        setStoredState(value); // Update stored state displayed on the screen
-        onDisplayNotification("Your Leave Application Status Updated");
-      }
-    } catch (e) {
-      // console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    // Run getData after presentState changes
-    if (presentState !== "") {
-      getData();
-    }
-  }, [presentState]);
-
-  const _storeData = async (status: string) => {
-    try {
-      await AsyncStorage.setItem("initial_status", status);
-      // console.log("Stored the Applied Status Successfully.");
-      setpresentState(status);
-
-      // Trigger notification if status is different from the stored value
-      if (status !== (await AsyncStorage.getItem("initial_status"))) {
-        onDisplayNotification("Your Leave Application Status Updated");
-      }
-    } catch (e) {
-      // console.log(e);
-    }
-  };
 
   return (
     <SafeAreaView>
@@ -82,10 +26,6 @@ function Notification(): React.JSX.Element {
             updates on the approval or rejection status of your leave requests
             and bank details submissions.
           </Text>
-          {/* /tle="Apply for Leave (Dummy)" onPress={() => setpresentState("Applied")} color="#2D0C8B"/> */}
-        </View>
-        <View style={{ margin: 50 }}>
-          {/* <Button title="Update Status (Dummy)" onPress={() => setpresentState("Approved")} color="#2D0C8B"/> */}
         </View>
       </ScrollView>
     </SafeAreaView>
